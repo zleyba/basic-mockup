0902bfc2e3904bc67e5079a87263521927701849
zleyba <mtvzach@gmail.com>
2016-01-27 14:48:44 -0700

    bootstrap test

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<?php include_once "../basic-mockup/php/bootstrap_loader.php"?>
+		<link type="text/css" href="../basic-mockup/css/style.css" rel="stylesheet" />
+		<title>Boot Strap Test</title>
+	</head>
+	<body>
+		<body>
+			<div class="container">
+
+				<div class="row">
+					<div class="col-md-6"> Column One: 6/12 columns, 50% of container width. </div>
+					<div class="col-md-6"> Column Two: 6/12 columns, 50% of container width. </div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4"> Column Three: 4/12 columns, 33.33% of container width. </div>
+					<div class="col-md-4"> Column Four: 4/12 columns, 33.33% of container width. </div>
+					<div class="col-md-4"> Column Five: 4/12 columns, 33.33% of container width. </div>
+				</div>
+
+			</div>
+		</body>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -23,12 +23,14 @@
 							<li>Have fun! We will be going over each student's project in class. :D</li>
 						</ol>
 					</div>
+					<div class="content">
 					<img class="pull-left" src="img/star-trek-cat.jpg" alt="star trek cat" />
 					<h2>These are the voyages of the Starship Enterprise!</h2>
 					<p>Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent.</p>
 					<h3>Cmdr Riker's nervous system has been invaded by an unknown microorganism.</h3>
 					<p>Now what are the possibilities of warp drive? The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.</p>
 					<p>Communication is not possible. The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?</p>
+					</div>
 					<section class="clear">
 						<img class="pull-right" src="img/janeway.jpeg" alt="captain janeway is fierce" />
 						<h2>Shields up!</h2>
